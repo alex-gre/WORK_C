2aa7eaac1527f08fa7eb43d9c0712510e693cc94
alex-gre <40606584+alex-gre@users.noreply.github.com>
2024-10-27 12:36:42 +0300

    sizeof demo

---
+++ sizeof.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    printf(" Данные типа int занимают %d байта. \n", sizeof (int));
+    printf(" Данные типа char занимают %d байт.\n", sizeof (char));
+    printf(" Данные типа long занимают %d байта.\n", sizeof (long));
+    printf(" Данные типа double занимают %d байт.\n", sizeof (double));
+
+return 0;    
+}
